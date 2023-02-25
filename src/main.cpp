a7fa93b43ef487d183ec7c63e051798991ffa863
amogus <91487506+Czuowuek-SOS@users.noreply.github.com>
2023-02-25 10:33:16 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -188,7 +188,7 @@ int main()
 
 int getDistance(sf::Vector2f p1, sf::Vector2f p2)
 {
-    int distance = (p1.x - p2.x) - (p1.y - p2.y);
+    int distance = sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 
     return distance;
 }
